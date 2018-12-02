d50852341b189889be4b8a70a343888439704b26
chrissx <chrissx@chrissx.ga>
2018-12-02 01:05:49 +0100

    Added FTB, DOSBOX and Blender.

--- install.sh
+++ install.sh
@@ -8,16 +8,12 @@
 apt update
 
 ###### Install main apps, drivers and dependencies
-apt install -y ubuntu-drivers-common
-apt install -y ubuntu-restricted-extras
-apt install -y ttf-ubuntu-font-family
-apt install -y openssh-client curl gcc g++
-apt install -y libstartup-notification0
+apt install -y ubuntu-drivers-common ubuntu-restricted-extras
+apt install -y ttf-ubuntu-font-family openssh-client curl gcc g++
+apt install -y libstartup-notification0 clang clang++
 apt install -y evince evince-common make autoconf automake
-apt install -y x11-xserver-utils arandr
-apt install -y pavucontrol dconf-tools
-apt install -y python-gconf lxappearance
-apt install -y policykit-desktop-privileges
+apt install -y x11-xserver-utils arandr pavucontrol dconf-tools
+apt install -y python-gconf lxappearance policykit-desktop-privileges
 apt install -y policykit-1-gnome vim thunar
 apt install -y gnome-system-monitor gnome-system-tools
 apt install -y network-manager wireless-tools
@@ -26,23 +22,19 @@ apt install -y overlay-scrollbar overlay-scrollbar-gtk2
 apt install -y pulseaudio-module-x11
 apt install -y totem-plugins gromit gstreamer1.0-pulseaudio
 apt install -y transmission-gtk pulseaudio-utils xbacklight
-apt install -y feh rofi compton gnome-terminal
-apt install -y i3 i3-wm i3blocks i3lock i3status
-apt install -y xserver-xorg-video-intel
+apt install -y feh rofi compton gnome-terminal i3 i3-wm i3blocks
+apt install -y i3lock i3status xserver-xorg-video-intel hplip
 apt install -y acpi-support apport-gtk foomatic-db-compressed-ppds
-apt install -y hplip
 apt install -y evince evince-common libssh-4 libnm-glib-vpn1
 apt install -y dmz-cursor-theme libwayland-cursor0
-apt install -y libxcursor1 xcursor-themes
-apt install -y mousetweaks laptop-detect
+apt install -y libxcursor1 xcursor-themes mousetweaks laptop-detect
 apt install -y update-inetd update-notifier update-notifier-common
-apt install -y usb-creator-common usb-creator-gtk
-apt install -y gnome-power-manager
+apt install -y usb-creator-common usb-creator-gtk gnome-power-manager
 apt install -y libgsettings-qt1 libproxy1-plugin-gsettings
 apt install -y libappindicator3-1 gir1.2-appindicator3-0.1 gdebi
 snap install vscode --classic
 snap install dotnet-sdk --classic
-apt install -y zsh htop tmux
+apt install -y zsh htop tmux dosbox blender
 
 ###### Get and install playerctl
 curl -L -o pc.deb 'https://github.com/acrisci/playerctl/releases/download/v2.0.1/playerctl-2.0.1_amd64.deb'
@@ -84,6 +76,7 @@ snap remove gnome-characters
 cp -f tools/y /bin/y
 cp -f tools/ghk /bin/ghk
 cp -f tools/ghp /bin/ghp
+cp -f tools/ftb /bin/ftb
 vim ghk.txt
 openssl aes-256-cbc -a -in ghk.txt -out ~/.ghk.enc
 rm -f ghk.txt
@@ -91,3 +84,4 @@ find /sys/ -type f -iname '*brightness*' | grep intel
 echo please select your backlight without the /brightness in it
 read backlight
 sudo ln -s $backlight /sys/class/backlight
+curl -L -o ~/.ftb.jar http://ftb.cursecdn.com/FTB2/launcher/FTB_Launcher.jar
