6d4c0f94f738e427117ecdebb19d67f2242d46fc
chrissx <chrissx@chrissx.ga>
2019-06-01 20:50:56 +0200

    implemented the new ybacklight syntax

--- configs/i3/i3status1.sh
+++ configs/i3/i3status1.sh
@@ -2,5 +2,5 @@
 i3status -c ~/.config/i3/i3status | while :
 do
         read line
-	echo $line | sed "s/}]/},{\"name\":\"aha\",\"full_text\":\" 💻 $(ybacklight cur)\"}]/g"
+	echo $line | sed "s/}]/},{\"name\":\"aha\",\"full_text\":\" 💻 $(ybacklight Sc\\/Sm)\"}]/g"
 done
