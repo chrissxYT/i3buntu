626e3cfed3d53eec87e512f7982f975209d78d12
chrissx <chrissx@chrissx.ga>
2019-06-09 00:21:22 +0200

    merged all the i3status scripts into one and improved it a lot.

--- configs/i3/i3status1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-i3status -c ~/.config/i3/i3status | while :
-do
-        read line
-	echo $line | sed "s/}]/},{\"name\":\"aha\",\"full_text\":\" 💻 $(ybacklight Sc\\/Sm)\"}]/g"
-done
--- configs/i3/i3status2.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-~/.config/i3/i3status1.sh | sed "s/},{/},{\"name\":\"ellol\",\"full_text\":\"\",\"separator\":false,\"separator_block_width\":0},{/g"
