23f3ad3c1d3d0dc5dceb6451eb9458f07969f0bb
chrissx <chrissx@chrissx.ga>
2019-06-18 00:12:58 +0200

    removed fully charged batteries from i3status

--- configs/i3/i3status.sh
+++ configs/i3/i3status.sh
@@ -10,10 +10,14 @@ do
 		sed "s/ Mb\\/s/M/g" | \
 		sed "s/ Gb\\/s/G/g" | \
 		sed "s/( /(/g" | \
+		sed "s/ )/)/g" | \
+		sed "s/ ()//g" | \
+		sed "s/()//g" | \
 		sed "s/{\"name\":\"cpu_temperature\",\"instance\":\"\\/sys\\/devices\\/platform\\/coretemp.0\\/hwmon\\/hwmon1\\/temp1_input\",\"markup\":\"none\",\"full_text\":\"can't read temp\",\"separator\":false,\"separator_block_width\":0},//g" | \
 		sed "s/{\"name\":\"cpu_temperature\",\"instance\":\"\\/sys\\/devices\\/platform\\/coretemp.0\\/hwmon\\/hwmon2\\/temp1_input\",\"markup\":\"none\",\"full_text\":\"can't read temp\",\"separator\":false,\"separator_block_width\":0},//g" | \
 		sed "s/{\"name\":\"wireless\",\"instance\":\"wlp3s0\",\"color\":\"#FF0000\",\"markup\":\"none\",\"full_text\":\" wifi_down_remove_pls \",\"separator\":false,\"separator_block_width\":0},//g" | \
 		sed "s/{\"name\":\"battery\",\"instance\":\"\\/sys\\/class\\/power_supply\\/BAT0\\/uevent\",\"markup\":\"none\",\"full_text\":\" no_battery_remove_pls \",\"separator\":false,\"separator_block_width\":0},//g" | \
 		sed "s/{\"name\":\"ethernet\",\"instance\":\"eth0\",\"color\":\"#FF0000\",\"markup\":\"none\",\"full_text\":\" ethernet_down_remove_pls \",\"separator\":false,\"separator_block_width\":0},//g" | \
+		sed "s/{\"name\":\"battery\",\"instance\":\"\/sys\/class\/power_supply\/BAT0\/uevent\",\"markup\":\"none\",\"full_text\":\" . .*% 0,00W \",\"separator\":false,\"separator_block_width\":0},//g" | \
 		sed "s/},{/},{\"name\":\"ellol\",\"full_text\":\"\",\"separator\":false,\"separator_block_width\":0},{/g"
 done
