be55c676d8b74aecfdf8417c8d9e93dcc5d6700c
chrissx@ThinkBadly <chrissx@chrissx.ga>
2019-06-25 19:27:16 +0200

    shortened the new netsh a bit more

--- tools/netsh.c
+++ tools/netsh.c
@@ -3,46 +3,41 @@
 #include <string.h>
 #include <time.h>
 
-#define is_url(s) (!strncmp((s), "http://", 7) || \
-        !strncmp((s), "https://", 8) || !strncmp((s), "git://", 6))
+#define no_url(s) (strncmp((s), "http://", 7) && \
+        strncmp((s), "https://", 8) && strncmp((s), "git://", 6))
 
-char *rurl(char *u, char *bfr, int clone)
+char *rurl(char *u, char *bfr, int c)
 {
-        if(!is_url(u))
-        {
-                sprintf(bfr, clone ? "https://github.com/%s" :
-                        "https://github.com/%s/raw/master/netsh",
-                        u);
-                return bfr;
-        }
-        else return u;
+        sprintf(bfr, c ? "https://github.com/%s" :
+                "https://github.com/%s/raw/master/netsh",
+                u);
+        return bfr;
 }
 
 int main(int argc, char **argv)
 {
-        int clone = 0;
+        int c = 0;
         for(int i = 1; i < argc; i++)
         {
                 char *s = argv[i];
                 char bfr[strlen(s) + 1024];
                 char buf[strlen(s) + 1024];
-                if(!strcmp(s, "-c") || !strcmp(s, "--clone"))
-                        clone = !clone;
-                else if(clone)
+                if(!strcmp(s, "-c") || !strcmp(s, "--clone")) c = !c;
+                else if(c)
                 {
                         char dir[16];
                         sprintf(dir, ".netsh%4lx", time(0));
                         sprintf(bfr, "git clone '%s' %s; \
                                         cd %s; ./netsh; cd ..; \
                                         rm -rf %s",
-                                        rurl(s, buf, 1),
+                                        no_url(s)?rurl(s, buf, 1):s,
                                         dir, dir, dir);
                         system(bfr);
                 }
                 else
                 {
                         sprintf(bfr, "curl -L '%s' | sh",
-                                        rurl(s, buf, 0));
+                                        no_url(s)?rurl(s, buf, 0):s);
                         system(bfr);
                 }
         }
