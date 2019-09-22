2501b6ffb0d5e65feb1ffddfd68caf69050ccc09
chrissx@ThinkBadly <chrissx@chrissx.ga>
2019-09-22 13:18:11 +0200

    i3status: added support for hwmon numbers up to 4

--- configs/i3/i3status.sh
+++ configs/i3/i3status.sh
@@ -12,8 +12,7 @@ do
 		sed "s/ )/)/g" | \
                 sed "s/ ()//g" | \
                 sed "s/()//g" | \
-		sed "s/{\"name\":\"cpu_temperature\",\"instance\":\"\\/sys\\/devices\\/platform\\/coretemp.0\\/hwmon\\/hwmon1\\/temp1_input\",\"markup\":\"none\",\"full_text\":\"can't read temp\",\"separator\":false,\"separator_block_width\":0},//g" | \
-		sed "s/{\"name\":\"cpu_temperature\",\"instance\":\"\\/sys\\/devices\\/platform\\/coretemp.0\\/hwmon\\/hwmon2\\/temp1_input\",\"markup\":\"none\",\"full_text\":\"can't read temp\",\"separator\":false,\"separator_block_width\":0},//g" | \
+		sed "s/{\"name\":\"cpu_temperature\",\"instance\":\"\\/sys\\/devices\\/platform\\/coretemp.0\\/hwmon\\/hwmon[0-9]\\/temp1_input\",\"markup\":\"none\",\"full_text\":\"can't read temp\",\"separator\":false,\"separator_block_width\":0},//g" | \
 		sed "s/{\"name\":\"wireless\",\"instance\":\"wlp3s0\",\"color\":\"#FF0000\",\"markup\":\"none\",\"full_text\":\" wifi_down_remove_pls \",\"separator\":false,\"separator_block_width\":0},//g" | \
 		sed "s/{\"name\":\"battery\",\"instance\":\"\\/sys\\/class\\/power_supply\\/BAT0\\/uevent\",\"markup\":\"none\",\"full_text\":\" no_battery_remove_pls \",\"separator\":false,\"separator_block_width\":0},//g" | \
 		sed "s/{\"name\":\"ethernet\",\"instance\":\"eth0\",\"color\":\"#FF0000\",\"markup\":\"none\",\"full_text\":\" ethernet_down_remove_pls \",\"separator\":false,\"separator_block_width\":0},//g" | \
