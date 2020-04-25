68e0025a07eb0d39ae2581fb7685aa54ad9b5095
chrissx@ThinkBadly <chrissx@chrissx.de>
2020-04-25 16:48:03 +0200

    netsh: restructured and simplified some code

--- tools/netsh.c
+++ tools/netsh.c
@@ -3,15 +3,18 @@
 #include <string.h>
 #include <time.h>
 
-#define VERSION "1.6.1"
+#define NAME "netsh"
+#define YEARS "2019-2020"
+#define AUTHORS "Chris E. Häußler, chrissx Media"
+#define VERSION "1.6.2"
 
-#define HELP "netsh "VERSION"\n"\
-             "(c) 2019 Christian Erwin Häußler, chrissx Media\n"\
+#define HELP NAME" "VERSION"\n"\
+             "(c) "YEARS" "AUTHORS"\n"\
              "\n"\
              "Usage: netsh [OPTIONS] [URL/repo] [OPTIONS] [URL/repo] ... \n"\
              "\n"\
              "  --\n"\
-             "    Finish the options and treat all remaining arguments as repos/URLs.\n"\
+             "    Treat all remaining arguments as repos/URLs.\n"\
              "  -c, --clone\n"\
              "    Set or unset the clone flag.\n"\
              "  -f, --file\n"\
@@ -31,30 +34,32 @@
         !strncmp((s), "https://", 8) || !strncmp((s), "git://", 6) ||\
         !strncmp((s), "svn://", 6) || !strncmp((s), "cvs://", 6) || \
         !strncmp((s), "hg://", 5))
-#define EXEC(s) int __exec_rc = system(s); if(__exec_rc) printf("Got error from system(\"%s\"): %d\n", s, __exec_rc)
+
+#define clonecommand() sprintf(cmdbuf, \
+        "%s clone '%s' %s && cd %s && { %s; }; cd ..; rm -rf %s", \
+        scm, rurl(s, urlbuf, 1, f), fd, fd, cmd, fd)
+#define elsecommand()  sprintf(cmdbuf, \
+        "curl -Lo '%s' '%s'; chmod +x '%s'&& ./%s; rm -f %s", \
+        fd, rurl(s, urlbuf, 0, f), fd, fd, fd)
 
 char *rurl(char *u, char *bfr, int c, char *f)
 {
         if(url(u)) return u;
-        else if(!strncmp(u, "bb://", 5) && c) sprintf(bfr, "https://bitbucket.org/%s", u + 5);
+        else if(!strncmp(u, "bb://", 5) && c)
+                sprintf(bfr, "https://bitbucket.org/%s", u + 5);
         else sprintf(bfr, c ? "https://github.com/%s" :
                 "https://github.com/%s/raw/master/%s",
-                u, f);
+                strncmp(u, "gh://", 5) ? u : u + 5, f);
         return bfr;
 }
 
 int main(int argc, char **argv)
 {
         int c = 0, argsended = 0;
-        char *cmd = "./netsh";
-        char *f = "netsh";
-        char *scm = "git";
+        char *cmd = "./netsh", *f = "netsh", *scm = "git";
         for(int i = 1; i < argc; i++)
         {
                 char *s = argv[i];
-                char bfr[strlen(s) + 1024];
-                char buf[strlen(s) + 1024];
-                char fdb[16];
                 ARGSTART;
                 ARG(s, "c", "clone") c = !c;
                 ARG(s, "f", "file") f = argv[++i];
@@ -62,24 +67,16 @@ int main(int argc, char **argv)
                 ARG(s, "s", "scm") scm = argv[++i];
                 ARG(s, "v", "version") puts(VERSION);
                 ARG(s, "h", "help") puts(HELP);
-                else if(c)
-                {
-                        sprintf(fdb, ".netsh%4lx", time(0));
-                        sprintf(bfr, "%s clone '%s' %s && \
-                                        cd %s && %s; cd ..; \
-                                        rm -rf %s",
-                                        scm, rurl(s, buf, 1, f),
-                                        fdb, fdb, cmd, fdb);
-                        EXEC(bfr);
-                }
                 else
                 {
-                        sprintf(fdb, ".netsh%4lx", time(0));
-                        sprintf(bfr, "curl -Lo '%s' '%s'; \
-                                      chmod +x '%s'&& ./%s; rm -f %s",
-                                     fdb, rurl(s, buf, 0, f),
-                                     fdb, fdb, fdb);
-                        EXEC(bfr);
+                        char fd[16];
+                        char cmdbuf[strlen(s) + 512];
+                        char urlbuf[strlen(s) + 512];
+                        sprintf(fd, ".netsh%4lx", time(0));
+                        if(c) clonecommand();
+                        else   elsecommand();
+                        if(system(cmdbuf))
+                                printf("system(\"%s\") failed.\n", s);
                 }
         }
         return 0;
